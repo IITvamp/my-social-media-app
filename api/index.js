6d47f493f939a8d51477361cb88836f6e90005ce
Ayush Bansal <77586067+IITvamp@users.noreply.github.com>
2021-12-04 15:17:57 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -54,6 +54,6 @@ app.use("/api/posts", postRoute);
 app.use("/api/comment", commentRoute); 
 
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log("Backend server is running!");
 });
