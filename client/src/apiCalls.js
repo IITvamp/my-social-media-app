ab8b912f506fe242ce2a7e6ebf353c43f5ffbc0b
Ayush Bansal <77586067+IITvamp@users.noreply.github.com>
2021-12-04 16:57:02 +0530

    changing url

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const url = "https://obscure-meadow-29718.herokuapp.com/api";
+
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/auth/login", userCredential);
+    const res = await axios.post(`${url}/auth/login`, userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
