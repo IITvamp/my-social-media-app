738b59b0532a5bae97173621440d968e3d67ef7b
IITvamp <ayush_b@ece.iitr.ac.in>
2022-01-18 22:36:58 +0530

    changing cors policy

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ app.use(passport.initialize());
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.setHeader(
     "Access-Control-Allow-Methods",
